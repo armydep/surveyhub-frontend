7f09cb9b3c33156e201cc62ea0f664cc55eadd44
Arkady <orkasha@gmail.com>
2025-02-07 11:23:54 +0200

    route names

--- src/pages/ShowSurvey.jsx
+++ src/pages/ShowSurvey.jsx
@@ -42,59 +42,19 @@ export default function ShowSurvey() {
                 <h1>Survey: {survey.name}</h1>
                 <p><strong>Survey ID:</strong>{survey.surveyId}</p>
                 <p><strong>User ID:</strong>{survey.userId}</p>
+                <p><strong>Name:</strong>{survey.name}</p>
+                <p><strong>Description:</strong>{survey.description}</p>
+                <p><strong>Created:</strong>{new Date(survey.timestamp).toLocaleString()}</p>
             </div>
 
-            <h2>Questions</h2>
-            <ul className="questions-list">
-                <li>
-                    <div className="question-text">Is it lovely visit??</div>
-                    <div className="input-area">
-                        <textarea rows="2" readOnly>Yes/No</textarea>
-                    </div>
-                </li>
-                <li>
-                    <div className="question-text">How many lovely visits you have??</div>
-                    <div className="input-area">
-                        <textarea rows="2" readOnly>0 to 30</textarea>
-                    </div>
-                </li>
-                <li>
-                    <div className="question-text">Tell me a story</div>
-                    <div className="input-area">
-                        <textarea rows="4" readOnly>Enter your story here...</textarea>
-                    </div>
-                </li>
-                <li>
-                    <div className="question-text">What is your favored food</div>
-                    <div className="input-area">
-                        <textarea rows="2" readOnly>hamburger, pizza, hummus, fish</textarea>
-                    </div>
-                </li>
-            </ul>
-        </div>
-    )
-        ;
-
-};
-
-/*
-    return (
-        <div>
-            <h1>Survey</h1>
-            <p>Survey ID: {surveyId}</p>
+            <div>Questions</div>
+            {
+                survey.questions.map((q, index) => {
+                    console.log("Iter Question type: " + q.type + ". index: " + index);
+                    //const Component = questionComponents[q.type];
+                    //return Component(index, q);
+                })
+            }
         </div>
     );
-*/
-/*           <ul>
-                           {
-                               surveys.map(survey => (
-                                   <li key={survey.surveyId}>
-                                       <Link to={`/ShowSurvey/${survey.surveyId}`}>{survey.name}</Link>
-                                   </li>
-                               ))
-                           }
-                       </ul>
-                       <button onClick={() => window.location.href = '/CreateSurvey'}>
-                           Create Survey
-                       </button>
-           */
+};
