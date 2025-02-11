53f341e1681fa86f675e7ceaee4f2761b17a284a
Arkady <orkasha@gmail.com>
2025-02-12 00:40:06 +0200

    create event handle

--- config.js
+++ config.js
@@ -1,5 +1,7 @@
 
-export const BACKEND_API_URL = 'http://localhost:8080';
-export const BACKEND_WS_URL = 'ws://localhost:8080/ws/api';
+export const BACKEND_API_URL = 'http://http://armydep.duckdns.org';
+export const BACKEND_WS_URL = 'ws://http://armydep.duckdns.org/ws/api';
+// export const BACKEND_API_URL = 'http://localhost:8080';
+// export const BACKEND_WS_URL = 'ws://localhost:8080/ws/api';
 
 //export const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || 'http://api.mybackend.com';
