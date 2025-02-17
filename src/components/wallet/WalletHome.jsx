1d0c1f67985aad24b316a31ac7cae15e91e1486a
Arkady <orkasha@gmail.com>
2025-02-17 22:22:29 +0200

    wallet poc

--- src/components/wallet/WalletHome.jsx
+++ src/components/wallet/WalletHome.jsx
@@ -1,11 +1,5 @@
 import {useEffect, useState} from 'react';
 import {fetchWithAuth} from './wallet_api.jsx';
-import {Link, useNavigate} from 'react-router-dom';
-//import {deleteSurvey, listSurveys} from '../api/api.js';
-//import {BACKEND_WS_URL} from '../config.js';
-//import styles from '../styles/Home.module.css';
-//import {useNavigate} from 'react-router-dom';
-import styles from './WalletStyles.jsx';
 
 export default function WalletHome() {
     const [data, setData] = useState(null);
@@ -15,7 +9,7 @@ export default function WalletHome() {
         const fetchData = async () => {
             try {
                 //const result = await fetchWithAuth('/wallet/api/user/user');
-                const result = await fetchWithAuth('/wallet/api/operation/balance/15ZbsZw8zhSBToqBkAvdQzBjWeAg43htBf');
+                const result = await fetchWithAuth('/wallet/api/operation/balance');
                 setData(result);
             } catch (error) {
                 setError('Failed to fetch data: ' + error);
